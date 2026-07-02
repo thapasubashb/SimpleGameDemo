82a8a4b2cd14ece32e779899180c2e7adf7c1ca0
Subash B <thapasubash9072@gmail.com>
2026-07-02 11:26:12 +0530

    i commited one file because its not complete and is half done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-    whacAMole   whacAMole = new whacAMole();
-    }
-}
+// public class App {
+// public static void main(String[] args) throws Exception {
+// whacAMole whacAMole = new whacAMole();
+// }
+// }
+// there are still stuff to do in this simple game demo
